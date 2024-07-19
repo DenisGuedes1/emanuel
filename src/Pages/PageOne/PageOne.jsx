abdb7fc3cb7ae5057a5df352cb59f82fcdfb178a
DenisGuedes1 <denisguedesmoraes@gmail.com>
2024-07-18 21:13:17 -0300

    first commit

--- src/Pages/PageOne/PageOne.jsx
+++ src/Pages/PageOne/PageOne.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-// import { HeadersComponents } from "../../Components/Header/Header";
-// import { TextBody } from "../../Components/TextBody/textBody";
-// import FooterComponents from '../../Components/Footer/Footer';
+import { HeadersComponents } from "../../Components/Header/Header";
+import { TextBody } from "../../Components/TextBody/textBody";
+import FooterComponents from '../../Components/Footer/Footer';
 
 export const Page = ()=>{
     return(
-        // <>
-        // <HeadersComponents/>
-        // <TextBody/>
-        // <FooterComponents/>
-        // </>
-        <h1>Olá mundo</h1>
+        <>
+        <HeadersComponents/>
+        <TextBody/>
+        <FooterComponents/>
+        </>
+
     )
 }
